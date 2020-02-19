336cd4c92dc1e4f073b06fc8706c75a6f77efd1d
unknown <manut@A2502.labinfo.facens.br>
2020-02-18 21:56:18 -0300

    lorraine chapada

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Eae lorraine para de fqzer oq vc esta fazendo mano ta chapano quando vamo chapa?");
     }
 }
